3290c8035122c30512a02172adbbdc55e29f55a4
YJ <yjrenk9@github.com>
2026-03-13 00:56:57 -0400

    Did the practice exercise

--- scope_lab.js
+++ scope_lab.js
@@ -27,11 +27,33 @@ const functionConst = "I'm a block-scoped const";
 // runs perfectly
 show();
 
-console.log(functionVar); // Throws ReferenceError b/c it only exists in the show function
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError b/c it only exists in the show function
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
 
 // var is accessible in global and block scopes
 // if it's inside of a function, then it's only accessible in that function
 
 // let and const are accessible in the scope they're defined in
+
+{
+    var vars = '^';
+    let lets = 1;
+    const tau = 6.28;
+
+    console.log(vars, lets, tau);
+
+    vars = "mars";
+    lets = 0;
+    // tau = 3.14; // cause error b/c u can't reassign constant variables
+
+    console.log(vars, lets, tau);
+}
+
+// creates new variables of the same name due to being in a different scope (not constants)
+// same outcome even when you specify var, let, or const
+vars = "orange";
+lets = -1;
+tau = -6.28;
+
+console.log(vars, lets, tau);
